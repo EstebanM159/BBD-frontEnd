fe537866b2a0a81a652fd61e0ecad7dabb081096
EstebanM159 <estebanm91231@gmail.com>
2024-06-23 20:29:00 -0300

    Rearmado de rutas

--- src/router.tsx
+++ src/router.tsx
@@ -5,16 +5,18 @@ import DateForm from './components/DateForm'
 import RegisterView from './pages/auth/RegisterView'
 import LoginView from './pages/auth/LoginView'
 import AuthLayout from './layouts/AuthLayout'
+import Inicio from './pages/date/Inicio'
 
-export default function Router() {
+export default function Router () {
   return (
     <BrowserRouter>
       <Routes>
           <Route element={<AppLayout/>}>
-            <Route path="/" index element={<Home/>}/>
+            <Route path='/inicio' element={<Inicio/>}/>
             <Route path='/crear-turno' element={<DateForm/>}/>
           </Route>
           <Route element={<AuthLayout/>}>
+            <Route path="/" index element={<Home/>}/>
             <Route path='/auth/create-account' element={<RegisterView/>}/>
             <Route path='/auth/login' element={<LoginView/>}/>
           </Route>
