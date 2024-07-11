bd131a96820e767643ac846fe8d97f25076fece2
EstebanM159 <estebanm91231@gmail.com>
2024-07-11 11:34:12 -0300

    a

--- src/pages/date/Inicio.tsx
+++ src/pages/date/Inicio.tsx
@@ -18,11 +18,21 @@ export default function Inicio () {
                   <img className='rounded-full' src={data.picture.data.url} width={data.picture.data.width} height={data.picture.data.height}/>
                     )
             }
-
+        </div>
+        <div className='flex flex-col items-center w-full py-8 px-4 gap-4 bg-ship-gray-400 rounded-md mt-8'>
+          {/* Este div aparece si no hay turno */}
+          <div className='w-full flex flex-col gap-4 items-center justify-center md:flex-row md:justify-around'>
+            <h1 className='text-lg font-bold '>No solicitaste ningún turno</h1>
+            <button className='bg-envy-950 w-fit py-3 px-8 rounded-md text-ship-gray-50 font-normal text-xl hover:shadow-md hover:shadow-ship-gray-50'>Agendar turno</button>
+          </div>
+            <p className='text-start'>Horarios de la barbería: {' '}
+              <br className='md:hidden '/>
+              <span className='font-semibold'>13:00 hs a 20:00 hs</span>
+            </p>
         </div>
       </section>
 
-      <h1 className="pt-4 text-3xl">Noticias</h1>
+      {/* <h1 className="pt-4 text-3xl">Noticias</h1>
       <section className="pt-6">
         <div
           className="flex items-center px-4 justify-between border
@@ -30,7 +40,7 @@ export default function Inicio () {
             <img className="size-14" src="./desc.svg" alt="" />
             <h1>Hoy hay descuento del 25%</h1>
         </div>
-      </section>
+      </section> */}
     </>
 
     )
