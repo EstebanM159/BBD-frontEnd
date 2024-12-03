786c3ef47c8bcaef5f2f7464f2894ec364baec1b
EstebanM159 <estebanm91231@gmail.com>
2024-12-03 17:33:59 -0300

    cambios en router

--- src/router.tsx
+++ src/router.tsx
@@ -24,30 +24,31 @@ export default function Router () {
     <BrowserRouter>
       <Routes>
           <Route element={<AppLayout/>}>
-            <Route path='/inicio' element={<Inicio/>}/>
-            <Route path='/crear-turno' element={<DateForm/>}/>
-            <Route path='/editar-turno/:dateId' element={<EditDate/>}/>
-            <Route path='/404' element={<NotFound/>}/>
-            <Route element={<AdminLayout/>}>
-              <Route path='/admin' element={<AdminInicio/>}/>
-              {/* <Route path='/admin/auth/create-account' index element={<AdminRegisterView/>}/> */}
-              {/* <Route path='/admin/auth/login' index element={<LoginView/>}/> */}
+              <Route path='/inicio' element={<Inicio/>}/>
+              <Route path='/crear-turno' element={<DateForm/>}/>
+              <Route path='/editar-turno/:dateId' element={<EditDate/>}/>
+              <Route path='/404' element={<NotFound/>}/>
+              <Route element={<AdminLayout/>}>
+                <Route path='/admin' element={<AdminInicio/>}/>
+                {/* <Route path='/admin/auth/create-account' index element={<AdminRegisterView/>}/> */}
+                {/* <Route path='/admin/auth/login' index element={<LoginView/>}/> */}
+              </Route>
             <Route element={<ProfileLayout/>}>
               <Route path='/perfil' element={<Profile/>}/>
               <Route path='/perfil/cambiar-contraseña' element={<ChangePasswrod/>}/>
               <Route path='/perfil/editar-perfil' element={<EditProfile/>}/>
               <Route path='/perfil/metodos-de-pago' element={<PaymentMethod/>}/>
             </Route>
-          </Route>
+
           <Route element={<AuthLayout/>}>
-            <Route path="/" element={<Home/>} index/>
-            <Route path='/auth/crear-cuenta' element={<RegisterView/>}/>
-            <Route path='/auth/iniciar-sesion' element={<LoginView/>}/>
-            <Route path='/auth/recuperar-contraseña' element={<ForgotPasswordView/>}/>
-            <Route path='/auth/nueva-contraseña/:tokenId' element={<NewPasswordView/>}/>
+              <Route path="/" element={<Home/>} index/>
+              <Route path='/auth/crear-cuenta' element={<RegisterView/>}/>
+              <Route path='/auth/iniciar-sesion' element={<LoginView/>}/>
+              <Route path='/auth/recuperar-contraseña' element={<ForgotPasswordView/>}/>
+              <Route path='/auth/nueva-contraseña/:tokenId' element={<NewPasswordView/>}/>
+          </Route>
           </Route>
           <Route path="*" element={<Navigate to="/404"/>}/>
-        </Route>
       </Routes>
     </BrowserRouter>
   )
