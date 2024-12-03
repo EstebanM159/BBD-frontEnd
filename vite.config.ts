950d42c6763575b4a92e94608b0a5058286ea127
EstebanM159 <estebanm91231@gmail.com>
2024-12-03 17:03:28 -0300

    cambios

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { HttpProxy, defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()]
-
+  plugins: [react()]
 })
